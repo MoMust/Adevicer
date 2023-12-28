b9251c1ee6011ae2952597310add243e69f915a5
Mohammed Mustafa <68998715+MoMust@users.noreply.github.com>
2023-12-28 18:27:27 +0100

    Card for reviews created, adjusted responsivness for sideBAr, navbar and card (#2)

--- src/app/components/navbar/navbar.jsx
+++ src/app/components/navbar/navbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import styles from "./navbar.module.css";
+// import styles from "./navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className=" h-60 border relative box-border">
-      <div className="flex justify-between m-5">
-        <div className="">
+    <div className=" h-60 border relative box-border mainContainer">
+      <div className="flex gap-5 lg:justify-between m-5 contentContainer">
+        <div className="imageContainer">
           <Image
             src="/logo/AdvicerLogo.png"
             className="rounded-lg"
@@ -16,8 +16,8 @@ const Navbar = () => {
             alt=""
           />
         </div>
-        <div className="mt-10 flex text-center">
-          <h1 className="lg:text-5xl sm:text-3xl font-tomorrow">
+        <div className="mt-10 flex text-center headerContainer">
+          <h1 className="text-xl lg:text-5xl sm:text-3xl font-tomorrow">
             Welcome to the greatest{" "}
             <span className="bg-black text-white p-1 md:font-bold">
               Advicer
@@ -25,7 +25,7 @@ const Navbar = () => {
             <br></br>for you gadgets
           </h1>
         </div>
-        <div className="hidden md:flex gap-3 text-xl h-10 text-2xl">
+        <div className="hidden lg:flex gap-3 text-xl h-10 text-2xl linksContainer">
           <Link href="/" className="group">
             Home
             <div class="bg-black h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
@@ -39,7 +39,7 @@ const Navbar = () => {
             <div class="bg-black h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
           </Link>
         </div>
-        <button className="md:hidden absolute right-10 bottom-10">
+        <button className="lg:hidden absolute right-10 bottom-10">
           <Image src="/icons/hamMenu.svg" width={30} height={30} />
         </button>
       </div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,11 +3,109 @@ import Gadgets from "./components/showGadgets/page";
 import ReviewList from "./components/reviewList/reviewList";
 import Link from "next/link";
 import SideBar from "./components/sideBar/sideBar";
+
 export default function Home() {
   return (
     <div className=" grid grid-cols-10 min-h-screen">
-      <SideBar/>
-      <div className="border col-span-8">Content</div>
+      <SideBar />
+      <div className="font-tomorrow lg:col-span-8 col-span-10 flex flex-col items-center gap-14 justify-center card-container">
+        <div className="border rounded-md w-4/6 card">
+          <div className="pt-12">
+            <h1 className="text-center lg:text-3xl">Title</h1>
+          </div>
+          <div className=" bg-gray-100 flex mt-5 justify-center">
+            <Image src="/images/lenovo.png" width={250} height={250} />
+          </div>
+          <div className="flex justify-center mt-5">
+            <span>27-12-2023</span>
+          </div>
+          <div className="flex justify-center">
+            <div className="rounded flex flex-row gap-10 bg-gray-100 mt-4 p-3 pl-6 w-80 h-12">
+              <div className="flex gap-2">
+                <div>
+                  <Image src="/icons/heartGrey.png" width={20} height={20} />
+                </div>
+                <div className="text-center">
+                  <span className="">Like</span>
+                </div>
+              </div>
+              <div className="flex gap-2">
+                <div>
+                  <Image src="/icons/comment.png" width={20} height={20} />
+                </div>
+                <div className="text-center">
+                  <span>23 </span>
+                  <span>Comments</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="border rounded-md w-4/6 card">
+          <div className="pt-12">
+            <h1 className="text-center lg:text-3xl">Title</h1>
+          </div>
+          <div className=" bg-gray-100 flex mt-5 justify-center">
+            <Image src="/images/lenovo.png" width={250} height={250} />
+          </div>
+          <div className="flex justify-center mt-5">
+            <span>27-12-2023</span>
+          </div>
+          <div className="flex justify-center">
+            <div className="rounded flex flex-row gap-10 bg-gray-100 mt-4 p-3 pl-6 w-80 h-12">
+              <div className="flex gap-2">
+                <div>
+                  <Image src="/icons/heartGrey.png" width={20} height={20} />
+                </div>
+                <div className="text-center">
+                  <span className="hidden lg:block">Like</span>
+                </div>
+              </div>
+              <div className="flex gap-2">
+                <div>
+                  <Image src="/icons/comment.png" width={20} height={20} />
+                </div>
+                <div className="text-center hidden lg:block">
+                  <span>23 </span>
+                  <span>Comments</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="border rounded-md w-4/6 card">
+          <div className="pt-12">
+            <h1 className="text-center lg:text-3xl">Title</h1>
+          </div>
+          <div className=" bg-gray-100 flex mt-5 justify-center">
+            <Image src="/images/lenovo.png" width={250} height={250} />
+          </div>
+          <div className="flex justify-center mt-5">
+            <span>27-12-2023</span>
+          </div>
+          <div className="flex justify-center">
+            <div className="rounded flex flex-row gap-10 bg-gray-100 mt-4 p-3 pl-6 w-80 h-12">
+              <div className="flex gap-2">
+                <div>
+                  <Image src="/icons/heartGrey.png" width={20} height={20} />
+                </div>
+                <div className="text-center">
+                  <span className="hidden lg:block">Like</span>
+                </div>
+              </div>
+              <div className="flex gap-2">
+                <div>
+                  <Image src="/icons/comment.png" width={20} height={20} />
+                </div>
+                <div className="text-center hidden lg:block">
+                  <span>23 </span>
+                  <span>Comments</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
