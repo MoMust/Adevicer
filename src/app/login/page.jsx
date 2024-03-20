e933b25ff3ae9922c0659787acc17709576d0942
Mohammed Mustafa <68998715+MoMust@users.noreply.github.com>
2024-03-20 14:34:16 +0100

    Added sign in and sign out user via google (#11)

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,23 @@
+"use client";
 import React from "react";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import GoogleIcon from "@mui/icons-material/Google";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 
 const Page = () => {
+    const { data, status } = useSession();
+    const router = useRouter();
+    console.log(data, status);
+    if(status === "loading"){
+        return <div>Loading...</div>
+    }
+    if(status === "authenticated"){
+        router.push("/");
+    }
+
   return (
     <div className="flex justify-center border h-full font-tomorrow">
       <div className="relative flex flex-col justify-center content-center bg-zinc-900 lg:w-1/2 md:w-4/5 sm:w-full h-full mt-20 mb-20">
@@ -13,23 +27,26 @@ const Page = () => {
           </h1>
         </div>
         <div className="text-center flex flex-col h-full justify-center items-center gap-20 mb-20 mt-52">
-          <button className="flex flex-row justify-center items-center border lg:w-1/3 md:w-1/2  sm:w-1/2 h-20 bg-white text-black rounded-md">
+          <button
+            onClick={() => signIn("google")}
+            className="flex flex-row justify-center items-center border lg:w-1/3 md:w-1/2  sm:w-1/2 h-20 bg-white text-black rounded-md"
+          >
             <div className="pr-10">
               <GoogleIcon sx={{ fontSize: 40 }} />
             </div>
-            <div className="font-bold">Google</div>
+            <div className="font-bold">Login with Google</div>
           </button>
           <button className="flex flex-row justify-center items-center border lg:w-1/3  md:w-1/2 sm:w-1/2 h-20 bg-white text-black rounded-md">
             <div className="pr-10">
               <FacebookIcon sx={{ fontSize: 40 }} />
             </div>
-            <div className="font-bold">Facebook</div>
+            <div className="font-bold">Login with Facebook</div>
           </button>
           <button className="flex flex-row justify-center items-center border lg:w-1/3  md:w-1/2 sm:w-1/2 h-20 bg-white text-black rounded-md">
             <div className="pr-10">
               <GitHubIcon sx={{ fontSize: 40 }} />
             </div>
-            <div className="font-bold">Github</div>
+            <div className="font-bold">Login with Github</div>
           </button>
         </div>
         <div className="text-center flex h-24 text-white justify-center mb-20">
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,20 +1,24 @@
-import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./connect";
+import { getServerSession } from "next-auth";
+
 
 export const authOptions = {
   // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
+      clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
     // ...add more providers here
   ],
 };
 
-export default NextAuth(authOptions);
+export const getAuthSession = () => getServerSession (authOptions);
