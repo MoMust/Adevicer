aef55e05bdabac8c8b4b83dbf3450cea5fedd48b
Mohammd Mustafa <68998715+MoMust@users.noreply.github.com>
2024-06-25 22:06:52 +0200

    Updated login user information on landing page and responsivness, userIcon, name

--- src/app/components/navbar/navbar.jsx
+++ src/app/components/navbar/navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
   return (
     <div className=" h-68 lg:h-60 border relative box-border mainContainer">
-      <div className="flex gap-5 lg:justify-between m-5 contentContainer">
+      <div className="grid grid-cols-5 contentContainer">
         <div className="imageContainer">
           <Image
             src="/logo/AdvicerLogo.png"
@@ -23,8 +23,8 @@ const Navbar = () => {
             alt="logo"
           />
         </div>
-        <div className="mt-10 flex text-center headerContainer">
-          <h1 className="text-xl lg:text-5xl sm:text-3xl font-tomorrow">
+        <div className="col-span-3 mt-10 flex justify-center headerContainer">
+          <h1 className="text-center text-xl lg:text-5xl sm:text-3xl font-tomorrow">
             Welcome to the greatest{" "}
             <span className="bg-black text-white p-1 md:font-bold">
               Advicer
@@ -32,32 +32,59 @@ const Navbar = () => {
             <br></br>for you gadgets
           </h1>
         </div>
-        <div className="font-tomorrow mt-28 lg:flex gap-3 text-xl h-10 text-2xl linksContainer">
-          <Link href="/" className="group">
-            Home
-            <div className="bg-black h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
-          </Link>
+        <div className="flex flex-col gap-5 ">
           {status === "authenticated" ? (
-            <div className="flex gap-3 lg:flex-row sm:flex-col gap-3 text-xl h-10 text-2xl linksContainer">
-              <Link href="/" className="group">
-                Write
-                <div className="bg-black h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
-              </Link>
-              <Link className="group" href="#" onClick={signOut}>
-                Logout
-                <div className="cursor-pointer bg-black h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
-              </Link>
+            <div className="flex flex-col items-center gap-3 justify-center">
+              <div className="">
+                <span className="text-2xl font-tomorrow"> Welcome</span>
+              </div>
+              <div className="hidden md:block">
+                <Image
+                  src="/icons/userIcon.png"
+                  width={40}
+                  height={40}
+                  alt="userIcon"
+                />
+              </div>
+              <div className="bg-black text-white w-9/12 flex items-center justify-center">
+                <p className="text-center font-bold text-lg">
+                  {data?.user?.name}
+                </p>
+              </div>
             </div>
           ) : (
-            <Link href="/login" className="group">
-              Login
-            </Link>
+            <div className="flex items-center justify-center">
+              <span className="text-2xl font-tomorrow"> Welcome</span>
+            </div>
           )}
+
+          <div className="font-tomorrow lg:flex justify-center gap-3 text-xl h-10 text-2xl linksContainer">
+            <Link href="/" className="group text-sm md:text-xl">
+              Home
+              <div className="bg-black h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
+            </Link>
+            {status === "authenticated" ? (
+              <div className="flex gap-3 lg:flex-row sm:flex-col gap-3 text-xl h-10 text-2xl linksContainer">
+                <Link href="/" className="group text-sm md:text-xl">
+                  Write
+                  <div className="bg-black h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
+                </Link>
+                <Link
+                  className="group text-sm md:text-xl"
+                  href="#"
+                  onClick={signOut}
+                >
+                  Logout
+                  <div className="cursor-pointer bg-black h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
+                </Link>
+              </div>
+            ) : (
+              <Link href="/login" className="group text-sm md:text-xl">
+                Login
+              </Link>
+            )}
+          </div>
         </div>
-        <span className="text-2xl font-tomorrow">
-          {" "}
-          Welcome {data?.user?.name}
-        </span>
         {showModal && <ModalMenu setShowModal={setShowModal} />}
       </div>
       <button
